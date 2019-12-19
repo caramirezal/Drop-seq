235744f7952559a00a75fa992f4bc7ea43f888e1
Carlos Ramirez <bq_cramirez@newlinuxappl.bioquant.uni-heidelberg.de>
2019-12-19 12:26:19 +0100

    run alignment setup in bioquant cluster

--- src/scripts/run_pipe.sh
+++ src/scripts/run_pipe.sh
@@ -16,18 +16,27 @@
 base_dir=/net/data.isilon/ag-cherrmann/cramirez/Drop-seq/
 
 ## command for bioquant cluster
-bash $base_dir/src/scripts/create_Drop-seq_reference_metadata.sh \
-    -n data_int \
-    -r $base_dir/data/ref/Mus_musculus.GRCm38.dna.primary_assembly.fa \
-    -s Mus_musculus \
-    -g $base_dir/data/ref/Mus_musculus.GRCm38.98.gtf \
-    -a $base_dir/inst/STAR-2.7.3a/bin/Linux_x86_64/STAR \
-    -b $base_dir/inst/htslib-1.10.1/ \
-    -d $base_dir/inst/Drop-seq_tools-2.3.0/ \
-    -o $base_dir/data/metadata/ \
-    -i $base_dir/inst/samtools-1.10/
-    -v
+#bash $base_dir/src/scripts/create_Drop-seq_reference_metadata.sh \
+#    -n data_int \
+#    -r $base_dir/data/ref/Mus_musculus.GRCm38.dna.primary_assembly.fa \
+#    -s Mus_musculus \
+#    -g $base_dir/data/ref/Mus_musculus.GRCm38.98.gtf \
+#    -a $base_dir/inst/STAR-2.7.3a/bin/Linux_x86_64/STAR \
+#    -b $base_dir/inst/htslib-1.10.1/ \
+#    -d $base_dir/inst/Drop-seq_tools-2.3.0/ \
+#    -o $base_dir/data/metadata/ \
+#    -i $base_dir/inst/samtools-1.10/
+#    -v
 
+
+bash $base_dir/src/scripts/Drop-seq_alignment.sh \
+     -g $base_dir/data/metadata/STAR/ \
+     -r $base_dir/data/metadata/data_int.fa \
+     -d $base_dir/inst/Drop-seq_tools-2.3.0/ \
+     -o $base_dir/data/out/ \
+     -t $base_dir/data/temp_dir/ \
+     -s $base_dir/inst/STAR-2.7.3a/bin/Linux_x86_64/STAR \
+     $base_dir/data/bam/SC_20180831A_BGHK_SNARE_cDNA_S1.bam 
 
 
 
--- src/scripts/setup.sh
+++ src/scripts/setup.sh
@@ -1,3 +1,4 @@
+dropseq_dir=/net/data.isilon/ag-cherrmann/cramirez/Drop-seq/
 
 ## dowloading mouse genome fasta file
 #base_url=ftp://ftp.ensembl.org/pub/release-98/fasta/mus_musculus/dna/
@@ -8,11 +9,16 @@
 #wget --tries=5 ftp://ftp.ensembl.org/pub/release-98/gtf/mus_musculus/Mus_musculus.GRCm38.98.gtf.gz 
 #gunzip -d Mus_musculus.GRCm38.98.gtf.gz
 
-./create_Drop-seq_reference_metadata.sh \
-    -n test01 \
-    -r /media/ag-cherrmann/cramirez/Drop-seq/ref/Mus_musculus.GRCm38.dna.primary_assembly.fa \
-    -s Mus_musculus \
-    -g /media/ag-cherrmann/cramirez/Drop-seq/ref/Mus_musculus.GRCm38.98.gtf \
-    -a /media/ag-cherrmann/cramirez/STAR-2.7.3a/bin/Linux_x86_64/ \
-    -b /media/ag-cherrmann/cramirez/Drop-seq/src/scripts/ref/htslib-1.10.1/ \
-    -v -e
+## download picard tools
+#wget https://github.com/broadinstitute/picard/releases/download/2.21.5/picard.jar \
+#    -P inst/
+#    --tries=3
+
+java -jar inst/picard.jar FastqToSam \
+       F1=$dropseq_dir/data/testing_data/SC_20180831A_BGHK_SNARE_cDNA_S1_R1_001.fastq.gz \
+       F2=$dropseq_dir/data/testing_data/SC_20180831A_BGHK_SNARE_cDNA_S1_R2_001.fastq.gz \
+       O=$dropseq_dir/data/bam/SC_20180831A_BGHK_SNARE_cDNA_S1.bam \
+       SM=BGHK \
+       RG=20180831A
+       
+
