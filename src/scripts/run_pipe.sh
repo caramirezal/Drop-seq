5e08f85bef51e24720bdbb569eca6fcb04cc79d6
caramirezal <cramireza@ciencias.unam.mx>
2019-12-20 11:09:31 +0100

    digital expression construction

--- src/scripts/run_pipe.sh
+++ src/scripts/run_pipe.sh
@@ -1,20 +1,8 @@
-## command that works in curry cluster
-#./create_Drop-seq_reference_metadata.sh \
-#    -n test01 \
-#    -r /media/ag-cherrmann/cramirez/Drop-seq/src/scripts/ref/Mus_musculus.GRCm38.dna.primary_assembly.fa \
-#    -s Mus_musculus \
-#    -g /media/ag-cherrmann/cramirez/Drop-seq/src/scripts/ref/Mus_musculus.GRCm38.98.gtf \
-#    -a /media/ag-cherrmann/cramirez/STAR-2.7.3a/bin/Linux_x86_64/STAR \
-#    -b /media/ag-cherrmann/cramirez/Drop-seq/src/scripts/ref/htslib-1.10.1/ \
-#    -d /media/ag-cherrmann/cramirez/Drop-seq/src/scripts/Drop-seq_tools-2.3.0/ \
-#    -o out \
-#    -v
 
-#export JAVA_HOME=/net/data.isilon/ag-cherrmann/cramirez/miniconda3/envs/dropseq/jre/bin/
-#export PATH=$JAVA_HOME/bin:$PATH
-
+## paths and dependencies
 base_dir=/net/data.isilon/ag-cherrmann/cramirez/Drop-seq/
 
+## indexing and reference metadata construction
 ## command for bioquant cluster
 #bash $base_dir/src/scripts/create_Drop-seq_reference_metadata.sh \
 #    -n data_int \
@@ -28,15 +16,20 @@ base_dir=/net/data.isilon/ag-cherrmann/cramirez/Drop-seq/
 #    -i $base_dir/inst/samtools-1.10/
 #    -v
 
+## performing alignement
+#bash $base_dir/src/scripts/Drop-seq_alignment.sh \
+#     -g $base_dir/data/metadata/STAR/ \
+#     -r $base_dir/data/metadata/data_int.fasta \
+#     -d $base_dir/inst/Drop-seq_tools-2.3.0/ \
+#     -o $base_dir/data/out/ \
+#     -t $base_dir/data/temp_dir/ \
+#     -s $base_dir/inst/STAR-2.7.3a/bin/Linux_x86_64/STAR \
+#     $base_dir/data/bam/SC_20180831A_BGHK_SNARE_cDNA_S1.bam 
 
-bash $base_dir/src/scripts/Drop-seq_alignment.sh \
-     -g $base_dir/data/metadata/STAR/ \
-     -r $base_dir/data/metadata/data_int.fasta \
-     -d $base_dir/inst/Drop-seq_tools-2.3.0/ \
-     -o $base_dir/data/out/ \
-     -t $base_dir/data/temp_dir/ \
-     -s $base_dir/inst/STAR-2.7.3a/bin/Linux_x86_64/STAR \
-     $base_dir/data/bam/SC_20180831A_BGHK_SNARE_cDNA_S1.bam 
-
-
+## creating digital expression matrix
+bash $base_dir/inst/Drop-seq_tools-2.3.0/DigitalExpression \
+      I=$base_dir/data/out/final.bam \
+      O=$base_dir/data/matrix.mtx.gz \
+      SUMMARY=out_gene_exon_tagged.dge.summary.txt \
+      NUM_CORE_BARCODES=100
 
